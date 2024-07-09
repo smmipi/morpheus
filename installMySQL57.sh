f43662745db2ab8feac98faeda0363a8328ed7a3
smmipi <41748146+smmipi@users.noreply.github.com>
2024-07-09 18:39:31 +0200

    Update installMySQL57.sh

--- installMySQL57.sh
+++ installMySQL57.sh
@@ -6,7 +6,7 @@ wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.
 dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
 apt-get update -y
 
-DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 
 echo "percona-server-server-5.7 percona-server-server-5.7/root-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
 echo "percona-server-server-5.7 percona-server-server-5.7/re-root-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
