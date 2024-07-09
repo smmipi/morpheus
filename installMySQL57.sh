960644833d08e217b9548225d2eafb010a8fb7c0
smmipi <41748146+smmipi@users.noreply.github.com>
2024-07-09 21:19:42 +0200

    Update installMySQL57.sh

--- installMySQL57.sh
+++ installMySQL57.sh
@@ -1,12 +1,10 @@
 MYSQL_ROOT_PASSWORD="<%=cypher.read('secret/mysql_root')%>"
-
+DEBIAN_FRONTEND=noninteractive
 apt-get remove apparmor -y
 apt-get install gnupg2 -y
 wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
 dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
 apt-get update -y
-
-export DEBIAN_FRONTEND=noninteractive
 
 echo "percona-server-server-5.7 percona-server-server-5.7/root-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
 echo "percona-server-server-5.7 percona-server-server-5.7/re-root-pass password $MYSQL_ROOT_PASSWORD" | debconf-set-selections
